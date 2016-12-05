bc263cb1624de86c389e5520ba00d41cd3827cd8
melanholy <irrygan@gmail.com>
2016-12-05 22:03:46 +0500

    initial commit

---
+++ api.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var https = require('https');
+var fs = require('fs');
+
+var API_TOKEN = fs.readFileSync('token.txt', 'ascii').trim();
+var API_HOST = 'api.github.com';
+
+function makeRequest(options, headers, isJson, callback) {
+    options = Object.assign({}, options, {
+        hostname: API_HOST
+    });
+    options.headers = Object.assign({}, headers, {
+        'User-Agent': 'node.js'
+    });
+
+    var request = https.request(options);
+
+    request.on('response', function (response) {
+        var body = '';
+
+        response.on('data', function (chunk) {
+            body += chunk;
+        });
+
+        response.on('end', function () {
+            if (isJson) {
+                var result = JSON.parse(body);
+
+                if ('message' in result) {
+                    callback(new Error(result.message));
+                } else {
+                    callback(null, result);
+                }
+            } else {
+                callback(null, body);
+            }
+
+        });
+    });
+    request.on('timeout', function () {
+        callback(new Error('timeout'));
+    });
+    request.on('error', function () {
+        callback(new Error('network error'));
+    });
+
+    return request;
+}
+
+exports.getOrganizationRepos = function (organization, callback) {
+    var path = '/orgs/' + organization + '/repos?access_API_TOKEN=' + API_TOKEN;
+
+    var request = makeRequest({ path: path }, {}, true, callback);
+    request.end();
+};
+
+exports.getRepo = function (owner, repo, callback) {
+    var path = '/repos/' + owner + '/' + repo + '?access_API_TOKEN=' + API_TOKEN;
+
+    var request = makeRequest({ path: path }, {}, true, callback);
+    request.end();
+};
+
+exports.getReadme = function (owner, repo, callback) {
+    var path = '/repos/' + owner + '/' + repo + '/readme?access_API_TOKEN=' + API_TOKEN;
+
+    var request = makeRequest({ path: path }, {}, true, callback);
+    request.end();
+};
+
+exports.renderMarkdown = function (text, callback) {
+    var options = {
+        path: '/markdown/raw?access_API_TOKEN=' + API_TOKEN,
+        method: 'POST'
+    };
+    var headers = {
+        'Content-Length': Buffer.byteLength(text),
+        'Content-Type': 'text/x-markdown'
+    };
+    var request = makeRequest(options, headers, false, callback);
+
+    request.write(text);
+    request.end();
+};
--- tasks.js
+++ tasks.js
@@ -1,10 +1,15 @@
 'use strict';
 
+var api = require('./api.js');
+var flow = require('flow.js');
+
 /**
  * Сделано задание на звездочку
  * Реализовано получение html
  */
 exports.isStar = true;
+
+var ALLOWED_CATEGORIES = ['demo', 'javascript', 'markup'];
 
 /**
  * Получение списка задач
@@ -12,7 +17,32 @@ exports.isStar = true;
  * @param {Function} callback
  */
 exports.getList = function (category, callback) {
-    console.info(category, callback);
+    if (ALLOWED_CATEGORIES.indexOf(category) === -1) {
+        callback(1);
+
+        return;
+    }
+
+    api.getOrganizationRepos('urfu-2016', function (error, repos) {
+        if (error) {
+            callback(error);
+
+            return;
+        }
+
+        var result = repos
+            .filter(function (repo) {
+                return repo.name.startsWith(category + '-task');
+            })
+            .map(function (repo) {
+                return {
+                    name: repo.name,
+                    description: repo.description
+                };
+            });
+
+        callback(null, result);
+    });
 };
 
 /**
@@ -21,5 +51,59 @@ exports.getList = function (category, callback) {
  * @param {Function} callback
  */
 exports.loadOne = function (task, callback) {
-    console.info(task, callback);
+    flow.serial([
+        function (next) {
+            api.getRepo('urfu-2016', task, function (error, repo) {
+                if (error) {
+                    next(error);
+
+                    return;
+                }
+
+                next(null, repo);
+            });
+        },
+
+        function (repo, next) {
+            api.getReadme('urfu-2016', task, function (error, readme) {
+                if (error) {
+                    next(error);
+
+                    return;
+                }
+
+                next(null, {
+                    repo: repo,
+                    readme: new Buffer(readme.content, readme.encoding).toString('utf-8')
+                });
+            });
+        },
+
+        function (data, next) {
+            api.renderMarkdown(data.readme, function (error, html) {
+                if (error) {
+                    next(error);
+
+                    return;
+                }
+
+                data.html = html;
+                next(null, data);
+            });
+        }
+    ], function (error, data) {
+        if (error) {
+            callback(error);
+
+            return;
+        }
+
+        callback(null, {
+            name: data.repo.name,
+            description: data.repo.description,
+            markdown: data.readme,
+            html: data.html
+        });
+    });
+
 };
