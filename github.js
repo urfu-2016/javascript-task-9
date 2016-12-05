71a9def861c7ec77b2f1149cce2c1a6b1e29af14
AtnesNess <atnesness@gmail.com>
2016-12-05 19:25:51 +0500

    initial

---
+++ github.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var fs = require('fs');
+var http = require('https');
+var token = fs.readFileSync('token.txt', 'utf-8');
+
+
+function handleError(req, callback) {
+    req.on('error', function (err) {
+        callback(err, null);
+    });
+}
+
+function httpplain(callback, res) {
+    var body = '';
+    res.on('data', function (chunk) {
+        body += chunk;
+    });
+    res.on('end', function () {
+        callback(null, body);
+    });
+}
+
+function httpJson(callback, res) {
+    httpplain(function (err, body) {
+        if (err) {
+            callback(err, null);
+
+            return;
+        }
+        try {
+            callback(null, JSON.parse(body));
+        } catch (e) {
+            callback(e, null);
+        }
+    }, res);
+}
+
+exports.repoList = function (callback) {
+    var options = {
+        protocol: 'https:',
+        host: 'api.github.com',
+        path: '/orgs/urfu-2016/repos?access_token=' + token,
+        method: 'GET',
+        headers: { 'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)' }
+    };
+    var req = http.request(options, httpJson.bind(null, callback));
+    handleError(req, callback);
+    req.end();
+};
+
+exports.repoInfo = function (repo, callback) {
+    var options = {
+        protocol: 'https:',
+        host: 'api.github.com',
+        path: '/repos/urfu-2016/' + repo + '?access_token=' + token,
+        method: 'GET',
+        headers: { 'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)' }
+    };
+    var req = http.request(options, httpJson.bind(null, callback));
+    handleError(req, callback);
+    req.end();
+};
+
+
+exports.markdownToHTML = function (md, callback) {
+    var postData = String(md);
+    var options = {
+        protocol: 'https:',
+        host: 'api.github.com',
+        path: '/markdown/raw',
+        method: 'POST',
+        headers: { 'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+            'Content-Length': Buffer.byteLength(postData),
+            'Content-Type': 'text/x-markdown'
+        }
+    };
+    var req = http.request(options, httpplain.bind(null, callback));
+    handleError(req, callback);
+    req.write(postData);
+    req.end();
+};
--- tasks.js
+++ tasks.js
@@ -4,7 +4,31 @@
  * Сделано задание на звездочку
  * Реализовано получение html
  */
+
+var github = require('./github');
+var flow = require('flow');
 exports.isStar = true;
+
+var JS_TEMPLATE = /javascript-task-\d+/;
+var MARKUP_TEMPLATE = /markup-task-\d+/;
+var DEMO_TEMPLATE = /demo-task-\d+/;
+
+function getTemplate(category) {
+    if (category === 'javascript') {
+
+        return JS_TEMPLATE;
+    }
+    if (category === 'markup') {
+
+        return MARKUP_TEMPLATE;
+    }
+    if (category === 'demo') {
+
+        return DEMO_TEMPLATE;
+    }
+
+    throw new Error('Wrong category');
+}
 
 /**
  * Получение списка задач
@@ -12,8 +36,62 @@ exports.isStar = true;
  * @param {Function} callback
  */
 exports.getList = function (category, callback) {
-    console.info(category, callback);
+    var template = getTemplate(category);
+    github.repoList(function (err, res) {
+        if (!err) {
+            var tasks = [];
+            res.forEach(function (repo) {
+                if (repo.name.match(template)) {
+                    tasks.push({
+                        'name': repo.name,
+                        'description': repo.description
+                    });
+                }
+            });
+            callback(err, tasks);
+        } else {
+            callback(err, null);
+        }
+    });
 };
+
+function addData(func, task, fields, addFields) {
+    if (!addFields) {
+        addFields = fields;
+    }
+
+    return function (data, callback) {
+        func(task, function (err, jsonResponse) {
+            if (err) {
+                callback(err, null);
+
+                return;
+            }
+            for (var i = 0; i < fields.length; i++) {
+                data[addFields[i]] = jsonResponse[fields[i]];
+            }
+            callback(null, data);
+        });
+    };
+}
+
+function contentToMarkdown(data, callback) {
+    data.markdown = new Buffer(data.content, data.encoding).toString('utf-8');
+    delete data.content;
+    delete data.encoding;
+    callback(null, data);
+}
+
+
+function addHTMLMarkdown(data, callback) {
+    github.markdownToHTML(data.markdown, function (err, res) {
+        if (err) {
+            callback(err, null);
+        }
+        data.html = res;
+        callback(null, data);
+    });
+}
 
 /**
  * Загрузка одной задачи
@@ -21,5 +99,18 @@ exports.getList = function (category, callback) {
  * @param {Function} callback
  */
 exports.loadOne = function (task, callback) {
-    console.info(task, callback);
+    // console.info(task, callback);
+    // github.repoInfo(task, function (err, res) {
+    // console.log(err, res);
+    // });
+    var repoData = { 'name': task };
+    flow.serial([
+        addData(github.repoInfo, task, ['name', 'description']).bind(null, repoData),
+        addData(github.repoInfo, task + '/readme', ['content', 'encoding']),
+        contentToMarkdown,
+        addHTMLMarkdown
+
+    ], function (err, data) {
+        callback(err, data);
+    });
 };
