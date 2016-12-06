e67d101c93e12fd743889c16045ef58309d8a516
Gallyam Biktahsev <gallyamb@gmail.com>
2016-12-06 09:50:04 +0500

    Continue

--- GitHubClient.js
+++ GitHubClient.js
@@ -1,15 +1,44 @@
 'use strict';
 
+var https = require('https');
+var flow = require('flow.gallyam');
+var fs = require('fs');
 
-var http = require('http');
+var API_URL = 'api.github.com';
+var BASE_PATH = '/users/urfu-2016/repos';
 
-var BASE_URL = 'https://github.com/urfu-2016';
-
-function GitHubClient(username, token) {
-    this.username = username;
+function GitHubClient(token) {
     this.token = token;
 }
 
 exports.getRepos = function (category) {
-    http.get(BASE_URL + category)
+    flow.serial([
+            fs.readFile.bind(null, 'token.txt'),
+        ])
+    var options = {
+        hostname: API_URL,
+        port: 443,
+        path: BASE_PATH,
+        method: 'GET',
+        headers: {
+            'Authorization': 'Basic ' + new Buffer(':').toString('base64'),
+            'User-Agent': 'Gallyam repo browser'
+        }
+    };
+
+    var req = https.request(options,
+        function (res) {
+            console.log(res.statusCode);
+            var content = '';
+            res.on('data', function (chunk) {
+                content += chunk;
+            });
+            res.on('end', function () {
+                console.log('Content: ' + content);
+            });
+        }).on('error', function (err) {
+            console.log('Error: ' + err);
+        });
+
+    req.end();
 }
--- tmp.js
+++ tmp.js
@@ -1,7 +1,18 @@
-https.request('GET', 'https://api.github.com/zen',
-    {
-        'Authorization': 'Basic '
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+    hostname: 'api.github.com',
+    port: 443,
+    path: '/zen',
+    method: 'GET',
+    headers: {
+        'Authorization': 'Basic ' + new Buffer(':' + fs.readFileSync('token.txt')).toString('base64'),
+        'User-Agent': 'Gallyam repo browser'
     }
+};
+
+var req = https.request(options,
     function (res) {
         console.log(res.statusCode);
         var content = '';
@@ -14,3 +25,5 @@ https.request('GET', 'https://api.github.com/zen',
     }).on('error', function (err) {
         console.log('Error: ' + err);
     });
+
+req.end();
