14027042e4cabdcbf3527ad9323c57d565e9284a
Pavel <pp-1995@mail.ru>
2016-12-08 23:49:03 +0500

    Сделал задание

---
+++ githubApi.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var fs = require('fs');
+var https = require('https');
+
+var API_URL = 'api.github.com';
+var USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64)';
+var TOKEN = '';
+try {
+    TOKEN = fs.readFileSync('token.txt', { encoding: 'utf-8' }).replace(/\r|\n/g, '');
+    TOKEN = new Buffer(TOKEN).toString('ascii');
+} catch (e) {
+    console.info(e.message);
+}
+
+function getOptions(path) {
+    return {
+        host: API_URL,
+        path: path,
+        headers:
+        {
+            'Authorization': 'Bearer ' + TOKEN,
+            'User-Agent': USER_AGENT
+        }
+    };
+}
+
+function getResponseCallback(reject, resolve) {
+    return function (response) {
+        var data = '';
+        var hasError = false;
+        response.on('data', function (chunk) {
+            data += chunk;
+        });
+        response.on('error', function (err) {
+            reject(err);
+            hasError = true;
+        });
+        response.on('end', function () {
+            if (!hasError) {
+                resolve(data);
+            }
+        });
+    };
+}
+
+function jsonParse(data) {
+    var parsed = '';
+    try {
+        parsed = JSON.parse(data);
+    } catch (e) {
+        console.info(e.message);
+    }
+
+    return parsed;
+}
+
+exports.getRepos = function (org, callback) {
+    var orgPath = '/orgs/' + org + '/repos';
+    function resolve(data) {
+        callback(null, jsonParse(data));
+    }
+    https.get(getOptions(orgPath), getResponseCallback(callback, resolve)).end();
+};
+
+exports.getRepo = function (owner, repo, callback) {
+    var orgPath = '/repos/' + owner + '/' + repo;
+    function resolve(data) {
+        callback(null, jsonParse(data));
+    }
+    https.get(getOptions(orgPath), getResponseCallback(callback, resolve)).end();
+};
+
+exports.getMarkdown = function (owner, repo, callback) {
+    var orgPath = '/repos/' + owner + '/' + repo + '/readme';
+    function resolve(data) {
+        var parsed = jsonParse(data);
+        var content = '';
+        try {
+            content = new Buffer(parsed.content, 'base64').toString('utf-8');
+        } catch (e) {
+            console.info(e.message);
+        }
+        callback(null, content);
+    }
+    https.get(getOptions(orgPath), getResponseCallback(callback, resolve)).end();
+};
+
+exports.postHtmlMarkdown = function (markdown, callback) {
+    var options = getOptions('/markdown/raw');
+    options.headers['Content-Type'] = 'text/plain';
+    options.method = 'POST';
+    function contentResolve(data) {
+        callback(null, data);
+    }
+    var request = https.request(options, getResponseCallback(callback, contentResolve));
+    request.write(markdown);
+    request.end();
+};
--- tasks.js
+++ tasks.js
@@ -6,13 +6,34 @@
  */
 exports.isStar = true;
 
+var ORG = 'urfu-2016';
+var githubApi = require('./githubApi');
+var flow = require('flow');
+
 /**
  * Получение списка задач
  * @param {String} category – категория задач (javascript или markup)
  * @param {Function} callback
  */
 exports.getList = function (category, callback) {
-    console.info(category, callback);
+    githubApi.getRepos(ORG, function (err, repos) {
+        if (err) {
+            callback(err);
+        }
+        var tasks = repos.map(function (repo) {
+            return {
+                name: repo.name,
+                description: repo.description
+            };
+        })
+        .filter(function (task) {
+            return task.name.indexOf(category + '-task') !== -1;
+        })
+        .sort(function (a, b) {
+            return a.name.localeCompare(b.name);
+        });
+        callback(null, tasks);
+    });
 };
 
 /**
@@ -21,5 +42,21 @@ exports.getList = function (category, callback) {
  * @param {Function} callback
  */
 exports.loadOne = function (task, callback) {
-    console.info(task, callback);
+    flow.serial([
+        function (cb) {
+            githubApi.getRepo(ORG, task, cb);
+        },
+        function (data, cb) {
+            githubApi.getMarkdown(ORG, task, function (err, readmeData) {
+                data.markdown = readmeData;
+                cb(err, data);
+            });
+        },
+        function (data, cb) {
+            githubApi.postHtmlMarkdown(data.markdown, function (err, htmlReadmeData) {
+                data.html = htmlReadmeData;
+                cb(err, data);
+            });
+        }
+    ], callback);
 };
