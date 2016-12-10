96128031e1752162ea0ceef493ecef03f1478802
svl96 <vlasov.ft@gmail.com>
2016-12-11 02:12:13 +0500

    task 9

---
+++ flow.js
@@ -0,0 +1,157 @@
+'use strict';
+
+/**
+ * Сделано задание на звездочку
+ * Реализованы методы mapLimit и filterLimit
+ */
+exports.isStar = false;
+
+
+function makeAsync(func) {
+    return function () {
+        var args = [].slice.call(arguments);
+        var error = null;
+        var res = null;
+        var callback = args.pop();
+        try {
+            res = func.apply(null, args);
+        } catch (err) {
+            error = err;
+        }
+        callback(error, res);
+    };
+}
+
+function serial(operations, callback) {
+    var nextData;
+    var index = 0;
+    var nextFunc = function (err, data) {
+        if (err) {
+            callback(err);
+
+            return;
+        }
+        nextData = data;
+        index++;
+        nextOperation();
+    };
+    operations[index](nextFunc);
+    function nextOperation() {
+        if (index >= operations.length) {
+            callback(null, nextData);
+        } else {
+            operations[index](nextData, nextFunc);
+        }
+    }
+}
+
+function myMap(items, operation, callback) {
+    if (!items.length) {
+        callback(null, []);
+
+        return;
+    }
+    var resultArray = [];
+    var handleItemsCount = 0;
+    var returnedError = null;
+    items.forEach(function (item, index) {
+        operation(item, function (err, data) {
+            if (!returnedError) {
+                if (err) {
+                    returnedError = err;
+                    callback(err);
+                } else {
+                    resultArray[index] = data;
+                    handleItemsCount++;
+                }
+            }
+            if (handleItemsCount === items.length && !returnedError) {
+                callback(null, resultArray);
+            }
+        });
+    });
+}
+
+function myFilter(items, operation, callback) {
+    if (!items.length) {
+        callback(null, []);
+
+        return;
+    }
+    myMap(items, operation, function (err, data) {
+        if (err) {
+            callback(err);
+        } else {
+            var filtered = items.filter(function (item, index) {
+                return data[index];
+            });
+            callback(null, filtered);
+        }
+    });
+}
+
+/**
+ * Последовательное выполнение операций
+ * @param {Function[]} operations – функции для выполнения
+ * @param {Function} callback
+ */
+exports.serial = function (operations, callback) {
+    if (operations && operations.length > 0) {
+        serial(operations, callback);
+    } else {
+        callback(null, null);
+    }
+};
+
+/**
+ * Параллельная обработка элементов
+ * @param {Array} items – элементы для итерации
+ * @param {Function} operation – функция для обработки элементов
+ * @param {Function} callback
+ */
+exports.map = function (items, operation, callback) {
+    myMap(items, operation, callback);
+};
+
+/**
+ * Параллельная фильтрация элементов
+ * @param {Array} items – элементы для фильтрация
+ * @param {Function} operation – функция фильтрации элементов
+ * @param {Function} callback
+ */
+exports.filter = function (items, operation, callback) {
+    myFilter(items, operation, callback);
+};
+
+/**
+ * Асинхронизация функций
+ * @param {Function} func – функция, которой суждено стать асинхронной
+ * @returns {function} async func
+ */
+exports.makeAsync = function (func) {
+    return makeAsync(func);
+};
+
+/**
+ * Параллельная обработка элементов с ограничением
+ * @star
+ * @param {Array} items – элементы для итерации
+ * @param {Number} limit – максимальное количество выполняемых параллельно операций
+ * @param {Function} operation – функция для обработки элементов
+ * @param {Function} callback
+ */
+exports.mapLimit = function (items, limit, operation, callback) {
+    callback(new Error('Функция mapLimit не реализована'));
+};
+
+/**
+ * Параллельная фильтрация элементов с ограничением
+ * @star
+ * @param {Array} items – элементы для итерации
+ * @param {Number} limit – максимальное количество выполняемых параллельно операций
+ * @param {Function} operation – функция для обработки элементов
+ * @param {Function} callback
+ */
+exports.filterLimit = function (items, limit, operation, callback) {
+    callback(new Error('Функция filterLimit не реализована'));
+};
--- tasks.js
+++ tasks.js
@@ -4,7 +4,71 @@
  * Сделано задание на звездочку
  * Реализовано получение html
  */
-exports.isStar = true;
+exports.isStar = false;
+
+function checkData(data) {
+    if (JSON.parse(data).message === 'Not Found') {
+        throw new Error('Incorrect Repository');
+    }
+}
+
+function getRepositoryInfo(path, handleData, callback) {
+    var gitapi = require('./gitapi');
+    gitapi.repositoryRequest(path, function (err, data) {
+        if (err) {
+            callback(err);
+        } else {
+            var handledData;
+            var error;
+            try {
+                checkData(data);
+                handledData = handleData(data);
+            } catch (handleError) {
+                error = handleError;
+            }
+            callback(error, handledData);
+        }
+    });
+}
+
+function orgsRepositoriesHandle(repositoryProps, category) {
+    return function (data) {
+        var parsedData = JSON.parse(data).map(function (record) {
+            var newRecord = {};
+            repositoryProps.forEach(function (property) {
+                newRecord[property] = record[property];
+            });
+
+            return newRecord;
+        });
+
+        return parsedData.filter(function (record) {
+            return record.name.indexOf(category) !== -1;
+        });
+    };
+}
+
+function readmeInfoHandle() {
+    return function (data) {
+        var parsedData = JSON.parse(data);
+        var content = new Buffer(parsedData.content, 'base64')
+            .toString('utf8');
+
+        return { name: 'markdown', value: content };
+    };
+}
+
+function repositoryInfoHandle(repositoryProps) {
+    return function (data) {
+        var newRecord = {};
+        var record = JSON.parse(data);
+        repositoryProps.forEach(function (property) {
+            newRecord[property] = record[property];
+        });
+
+        return newRecord;
+    };
+}
 
 /**
  * Получение списка задач
@@ -12,7 +76,11 @@ exports.isStar = true;
  * @param {Function} callback
  */
 exports.getList = function (category, callback) {
-    console.info(category, callback);
+    var taskCategory = category + '-task';
+    var path = '/orgs/urfu-2016/repos';
+    getRepositoryInfo(path,
+        orgsRepositoriesHandle(['name', 'description'], taskCategory),
+        callback);
 };
 
 /**
@@ -21,5 +89,32 @@ exports.getList = function (category, callback) {
  * @param {Function} callback
  */
 exports.loadOne = function (task, callback) {
-    console.info(task, callback);
+    var flow = require('./flow');
+    flow.serial([
+        function (next) {
+            var path = '/repos/urfu-2016/' + task;
+            getRepositoryInfo(path,
+                repositoryInfoHandle(['name', 'description']),
+                function (err, data) {
+                    if (err) {
+                        next(err);
+                    } else {
+                        next(null, data);
+                    }
+                });
+        },
+        function (outputData, next) {
+            var path = '/repos/urfu-2016/' + task + '/readme';
+            getRepositoryInfo(path,
+                readmeInfoHandle(),
+                function (err, data) {
+                    if (err) {
+                        next(err);
+                    } else {
+                        outputData[data.name] = data.value;
+                        next(null, outputData);
+                    }
+                });
+        }
+    ], callback);
 };
