c3c6ed9f7f1f5e5b616ba964042b914a00af0187
Sergey Ubogov <sergeiubogov@mail.ru>
2016-12-11 02:45:33 +0500

    апи в отдельный файл

--- tasks.js
+++ tasks.js
@@ -1,53 +1,7 @@
 'use strict';
 
 exports.isStar = false;
-//  var requestsAPI = require('./requestsAPI');
-
-function getToken() {
-    var fs = require('fs');
-    var token = '';
-    try {
-        token = '?access_token=' + fs.readFileSync('token.txt', 'utf8');
-    } catch (err) {
-        console.info(1);
-    }
-
-    return token;
-}
-
-function getOptions(path) {
-    return {
-        protocol: 'https:',
-        host: 'api.github.com',
-        path: path + getToken(),
-        method: 'get',
-        headers: { 'user-agent': 'BratBratokBratishka' }
-    };
-}
-
-function request(path, call) {
-    var https = require('https');
-    var opt = getOptions(path);
-    var callback = function (response) {
-        var str = '';
-        response.on('data', function (chunk) {
-            str += chunk;
-        });
-        response.on('end', function () {
-            if (response.statusCode === 200) {
-                try {
-                    call(undefined, JSON.parse(str));
-                } catch (err) {
-                    call(err);
-                }
-            } else {
-                call(new Error(response.statusMessage));
-            }
-        });
-    };
-    https.request(opt, callback).end();
-}
-
+var requestsAPI = require('./api');
 
 /**
  * Получение списка задач
@@ -56,7 +10,7 @@ function request(path, call) {
  */
 exports.getList = function (category, callback) {
     var listTasks = [];
-    request('/orgs/urfu-2016/repos', function (err, data) {
+    requestsAPI.request('/orgs/urfu-2016/repos', function (err, data) {
         if (err) {
             callback(err);
 
@@ -78,8 +32,8 @@ exports.getList = function (category, callback) {
  */
 exports.loadOne = function (task, callback) {
     var result = {};
-    request('/repos/urfu-2016/' + task + '/readme', function (err1, data1) {
-        request('/repos/urfu-2016/' + task, function (err2, data2) {
+    requestsAPI.request('/repos/urfu-2016/' + task + '/readme', function (err1, data1) {
+        requestsAPI.request('/repos/urfu-2016/' + task, function (err2, data2) {
             if (err2) {
                 callback(err2, undefined);
 
