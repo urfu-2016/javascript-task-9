d6a2648051b014b4ba6cac821251b39b36a7a16d
Sergey Ubogov <sergeiubogov@mail.ru>
2016-12-11 00:18:47 +0500

    Add files via upload

--- tasks.js
+++ tasks.js
@@ -1,10 +1,7 @@
 'use strict';
 
-/**
- * Сделано задание на звездочку
- * Реализовано получение html
- */
-exports.isStar = true;
+exports.isStar = false;
+var requestsAPI = require('./requestsAPI');
 
 /**
  * Получение списка задач
@@ -12,7 +9,16 @@ exports.isStar = true;
  * @param {Function} callback
  */
 exports.getList = function (category, callback) {
-    console.info(category, callback);
+    var listTasks = [];
+    requestsAPI.request('/orgs/urfu-2016/repos', function (err, data) {
+        if (err) {
+            callback(err, data);
+        }
+        listTasks = data.filter(function (task) {
+            return task.name.indexOf('javascript-task-') >= 0;
+        });
+        callback(err, listTasks);
+    });
 };
 
 /**
@@ -21,5 +27,19 @@ exports.getList = function (category, callback) {
  * @param {Function} callback
  */
 exports.loadOne = function (task, callback) {
-    console.info(task, callback);
+    var result = {};
+    requestsAPI.request('/repos/urfu-2016/' + task + '/readme', function (err1, data1) {
+        requestsAPI.request('/repos/urfu-2016/' + task, function (err2, data2) {
+            if (err2) {
+                callback(err2, data2);
+            }
+            result.name = data2.name;
+            result.description = data2.description;
+            callback(undefined, result);
+        });
+        if (err1) {
+            callback(err1, data1);
+        }
+        result.markdown = new Buffer(data1.content, 'base64').toString('utf-8');
+    });
 };
