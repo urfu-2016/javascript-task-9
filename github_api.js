1d585bec66232d0246b859a58983ec61778eee5e
Artem Borzov <artem.borz@yandex.ru>
2016-12-11 21:06:45 +0500

    чёто сделал

---
+++ github_api.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var https = require('https');
+
+var API_HOST = 'api.github.com';
+var USER_AGENT = 'github-api-client';
+var PATH_SEPARATOR = '/';
+
+function createRequestOptions(path) {
+    return {
+        host: API_HOST,
+        path: PATH_SEPARATOR + path,
+        headers: { 'User-Agent': USER_AGENT }
+    };
+}
+
+function joinApiPath() {
+    return [].slice.call(arguments).join(PATH_SEPARATOR);
+}
+
+function fetchJson(path, callback) {
+    function fetchJsonCallback(response) {
+        var receivedChunks = [];
+
+        response.on('data', function (chunk) {
+            receivedChunks.push(chunk);
+        });
+
+        response.on('end', function () {
+            callback(null, JSON.parse(receivedChunks.join('')));
+        });
+    }
+
+    var options = createRequestOptions(path);
+    https.request(options, fetchJsonCallback)
+    .on('error', callback)
+    .end();
+}
+
+exports.getOrgRepos = function (org, callback) {
+    var reposPath = joinApiPath('orgs', org, 'repos');
+    fetchJson(reposPath, callback);
+};
+
+exports.getRepo = function (owner, repoName, callback) {
+    var repoPath = joinApiPath('repos', owner, repoName);
+    fetchJson(repoPath, callback);
+};
+
+exports.getReadme = function (owner, repoName, callback) {
+    var readmePath = joinApiPath('repos', owner, repoName, 'readme');
+
+    fetchJson(readmePath, function (err, readme) {
+        if (err) {
+            callback(err);
+
+            return;
+        }
+
+        try {
+            var readmeMd = new Buffer(readme.content, readme.encoding).toString('utf-8');
+            callback(null, readmeMd);
+        } catch (e) {
+            callback(e);
+        }
+    });
+};
+
+
