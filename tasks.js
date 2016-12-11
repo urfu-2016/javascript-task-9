233f342ae83407565ec7dd5623c25030b520eb78
danlunin <daniellunin@mail.ru>
2016-12-11 20:39:37 +0500

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -4,7 +4,9 @@
  * Сделано задание на звездочку
  * Реализовано получение html
  */
-exports.isStar = true;
+exports.isStar = false;
+var githubAPI = require('./githubAPI.js');
+var flow = require('flow');
 
 /**
  * Получение списка задач
@@ -12,7 +14,32 @@ exports.isStar = true;
  * @param {Function} callback
  */
 exports.getList = function (category, callback) {
-    console.info(category, callback);
+    var result;
+    if (category !== 'demo' && category !== 'javascript' && category !== 'markup') {
+        callback(new Error('Bad format'), null);
+    }
+    function extract(error, extracted) {
+        if (error) {
+            callback(error, null);
+        } else {
+            var template = category + '-task';
+            extracted = JSON.parse(extracted);
+            result = extracted
+                .filter(function (task) {
+                    return task.name.indexOf(template) !== -1;
+                })
+                .map(function (repository) {
+                    var note = {
+                        'name': repository.name,
+                        'description': repository.description
+                    };
+
+                    return note;
+                });
+            callback(error, result);
+        }
+    }
+    githubAPI.getList(extract);
 };
 
 /**
@@ -21,5 +48,38 @@ exports.getList = function (category, callback) {
  * @param {Function} callback
  */
 exports.loadOne = function (task, callback) {
-    console.info(task, callback);
+    flow.serial([
+        function (next) {
+            githubAPI.getRepository(task, function (error, extracted) {
+                if (!error) {
+                    extracted = JSON.parse(extracted);
+                    var note = {
+                        'name': extracted.name,
+                        'description': extracted.description
+                    };
+                    next(null, note);
+                } else {
+                    callback(error);
+                }
+            });
+        },
+        function (note, next) {
+            githubAPI.getReadMe(task, function (error, extracted) {
+                if (!error) {
+                    extracted = JSON.parse(extracted);
+                    var url = extracted.download_url;
+                    githubAPI.downloadReadMe(url, function (internalError, markdown) {
+                        if (!internalError) {
+                            note.markdown = markdown;
+                            next(null, note);
+                        } else {
+                            next(internalError);
+                        }
+                    });
+                } else {
+                    next(error);
+                }
+            });
+        }
+    ], callback);
 };
