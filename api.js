b4668e2d4eaa2594556971c2611bd63681a3f7b9
fakefeik <feikfake1@gmail.com>
2016-12-11 15:36:56 +0500

    Inline methods & constants

--- api.js
+++ api.js
@@ -3,27 +3,11 @@
 var https = require('https');
 var fs = require('fs');
 
-var GITHUB = 'api.github.com';
-var MARKDOWN_PATH = '/markdown/raw';
-var USER_AGENT = 'Yet-Another-Github-Api-App';
-
 var authorization = null;
 try {
     authorization = 'token ' + fs.readFileSync('token.txt', 'utf-8');
 } catch (e) {
     console.error(e);
-}
-
-function getReposPath(organization) {
-    return ['/orgs', organization, 'repos'].join('/');
-}
-
-function getRepoPath(organization, repo) {
-    return ['/repos', organization, repo].join('/');
-}
-
-function getRepoReadmePath(organization, repo) {
-    return [getRepoPath(organization, repo), 'readme'].join('/');
 }
 
 function addAuthorization(options) {
@@ -36,24 +20,20 @@ function addAuthorization(options) {
 
 function getOptions(path) {
     return addAuthorization({
-        host: GITHUB,
+        host: 'api.github.com',
         path: path,
         headers: {
-            'User-Agent': USER_AGENT
+            'User-Agent': 'Yet-Another-Github-Api-App'
         }
     });
 }
 
 function postOptions(path) {
-    return addAuthorization({
-        method: 'POST',
-        host: GITHUB,
-        path: path,
-        headers: {
-            'User-Agent': USER_AGENT,
-            'Content-Type': 'text/plain'
-        }
-    });
+    var options = getOptions(path);
+    options.method = 'POST';
+    options.headers['Content-Type'] = 'text/plain';
+
+    return options;
 }
 
 function getString(options, callback, data) {
@@ -93,17 +73,17 @@ function returnJson(callback) {
 }
 
 exports.getRepos = function (organization, callback) {
-    getString(getOptions(getReposPath(organization)), returnJson(callback));
+    getString(getOptions(['/orgs', organization, 'repos'].join('/')), returnJson(callback));
 };
 
 exports.getRepo = function (organization, repo, callback) {
-    getString(getOptions(getRepoPath(organization, repo)), returnJson(callback));
+    getString(getOptions(['/repos', organization, repo].join('/')), returnJson(callback));
 };
 
 exports.getRepoReadme = function (organization, repo, callback) {
-    getString(getOptions(getRepoReadmePath(organization, repo)), returnJson(callback));
+    getString(getOptions(['/repos', organization, repo, 'readme'].join('/')), returnJson(callback));
 };
 
 exports.getHtml = function (markdown, callback) {
-    getString(postOptions(MARKDOWN_PATH), callback, markdown);
+    getString(postOptions('/markdown/raw'), callback, markdown);
 };
